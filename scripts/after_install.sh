c7bfbd42a0981dd39d6f572d93a7bbdd6b0c486e
Rajapandian <161719716+Rajapandian2003@users.noreply.github.com>
2026-01-30 12:01:57 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /var/www/frontend
-systemctl restart nginx
+docker pull 977527528431.dkr.ecr.ap-south-1.amazonaws.com/frontend:latest
+docker run -d -p 9000:80 --name reactcontainer --restart unless-stopped 977527528431.dkr.ecr.ap-south-1.amazonaws.com/frontend:latest
